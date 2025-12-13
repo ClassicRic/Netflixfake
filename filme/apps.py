4c485e45e19e135abc430c6180449ba156772958
ClassicRic <ricardo.correia2003@hotmail.com>
2025-12-12 22:41:26 -0300

    apps ajuste

--- filme/apps.py
+++ filme/apps.py
@@ -1,30 +1,17 @@
 from django.apps import AppConfig
 import os
-from .models import User
+
 
 
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
+
     def ready(self):
         from .models import User
-
         email = os.getenv("EMAIL_ADMIN")
-        password = os.getenv("SENHA_ADMIN")
-
-        if not email or not password:
-            return
-
-        # ✅ checar username (campo único)
-        if User.objects.filter(username=email).exists():
-            return
-
-        User.objects.create_superuser(
-            username=email,
-            email=email,
-            password=password,
-            is_active=True,
-            is_staff=True,
-            is_superuser=True,
-        )
+        senha = os.getenv("SENHA_ADMIN")
+        usuarios = User.objects.filter(email=email)
+        if not usuarios: User.objects.create_superuser(username="admin", email=email, password=senha, is_active=True,
+                                                          is_staff=True, )
