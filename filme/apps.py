5e8f5ff2006dc7502310591260ce5f832dd34a4e
ClassicRic <ricardo.correia2003@hotmail.com>
2025-12-12 22:58:02 -0300

    apps ajuste

--- filme/apps.py
+++ filme/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 import os
+from django.db import IntegrityError
 
 
 
@@ -7,12 +8,3 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-
-    def ready(self):
-        from .models import User
-        email = os.getenv("EMAIL_ADMIN")
-        senha = os.getenv("SENHA_ADMIN")
-        usuarios = User.objects.filter(email=email)
-        if not usuarios:
-            User.objects.create_superuser(username="admin", email=email, password=senha, is_active=True,
-                                                          is_staff=True, )
